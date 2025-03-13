57052e7eb30b5e3a363a522141c6cdb5f24cf4b8
shamilbin <shamiltk029@gmail.com>
2025-03-13 16:15:16 +0530

    first day of js

---
+++ basic/one.js
@@ -0,0 +1,132 @@
+/* 
+
+
+// */
+
+// var a = 10;
+// console.log(a);
+
+// let b = 20;
+// console.log(b)
+
+// const c= 3;
+// console.log(c)
+
+
+
+// var name = "shamil";
+//  console.log(name)
+
+//  var age =22;
+
+//  console.log(age);
+
+//  var isAlive =true;
+//  console.log(isAlive)
+
+
+//Redeclare and Reassign
+
+// var a=50;
+// a=100;
+// console.log(a)
+
+// let b= 20;
+// b=40;
+// console.log(b);
+
+// const c = 60;
+//  c= c++;
+// console.log(c)
+
+
+// var is the first way of using till 2015
+// 
+
+
+
+/* 
+
+Data type in javascript
+
+There are two type of data type in js
+
+
+1. Primitive 
+2. Non Primitive
+
+Primitive :- Can store only one type of Data
+Non Primitive :-  Can store Multiple type of data type
+
+1 Primitive :
+    
+   a. String ==> textual data.    er: "shamil" 
+   b. Boolean ==>any of two variable
+   c. number ==> any integer of floating value
+   d. Null ==> denotes null or an empty value
+   e. Undefined ==> a datatype as there variable is not defined yet 
+
+
+
+*/
+
+// Example of string
+
+// let firstName = "shamil";
+// let lastName  = "TK"
+
+// // Bolean
+
+// let isAlive = true;
+// let isValid = false ;
+
+// // NUmber
+
+// let age = 20;
+// let number = 54.34 ;
+// let pie = 3.14 ;
+   
+
+// // Null 
+
+// let box = null ;
+// box =20 ;
+// console.log(box);
+
+// // Undefined
+
+// let box1 ;
+// console.log(box1)
+
+/*
+ Non Primitive data type
+
+1. Object 
+2. Array
+
+Object in javascript
+
+
+// */
+
+// let person = {
+//     fname : "shamil",
+//     age : 23 ,
+//     isAlive : true ,
+//     newPhone : null
+
+// }
+// // console.log(person)
+
+// let student =["shamil ", "suhail",23, null , true];
+
+// console.log(student[3])
+
+let student ={
+    fname : "shamil",
+    age: 23,
+    grade: "10th",
+    rollNumber : 34
+}
+
+console.table(student)
---
+++ basic/two.js
@@ -0,0 +1,66 @@
+/* 
+
+Operators in javascript
+
+
+in javascript an operator is a special type of symbol used to 
+perform operation on operands 
+ex : variable and values
+
+ex: 2+3 =5
+
+
+
+javaScript operators
+
+
+  1. assigntment operators
+
+  =        assignment Operator              a=7 ==> 7
+  +=       addition assigntment             a+= 5 ==> a=a+5
+  -=        subtraction assignment.         a-= 5. ==> a = a-5
+
+
+
+
+Arithmetic operator
+
+arithmatic opeator are used to perfome arithmatic calculation
+
+operation           Name               Example
+
+  +                  Addition           a + b =c
+  -                 Subtraction         a - b
+  *                 Multiplication      a * b
+  /                 Division            a / b
+  %                 Reminder            a % b
+  ++
+  --
+  **
+  
+
+
+
+
+
+
+*/
+
+
+// let person = {
+//         fname : "shamil",
+//         age : 23 ,
+//         isAlive : true ,
+//         newPhone : null
+    
+//     }
+
+//     console.log(typeof person);
+//     console.log(typeof person.age);
+//     console.log(typeof person.fname);
+
+
+let a1 ="shamil"
+let b1 = "TK"
+
+console.log(a1 +" " + b1)
