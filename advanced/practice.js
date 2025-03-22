9c9315d4cd4fee39c58fbd0a79556d9f4c612fca
shamilbin <shamiltk029@gmail.com>
2025-03-22 19:35:20 +0530

    add

--- advanced/practice.js
+++ advanced/practice.js
@@ -4,4 +4,17 @@ let person = {
   place: `kavanoor`,
 };
 
-let student=[`Hisham`,29,true,null,"kavanoor"]
+let student = [`Hisham`, 29, true, null, "kavanoor"];
+
+console.log(person)
+console.log(student)
+
+//  CRUD operaton in both object and array
+
+//      ADD
+
+person.city = "kerlala";
+student.unshift("India");
+
+console.log(person)
+console.log(student)
